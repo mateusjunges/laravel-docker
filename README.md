280c7c87d22817dc9bbccddd1dc8349d8be96509
mateusjunges <mateus@junges.dev>
2021-12-19 03:02:27 -0300

    wip

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ To build this image, you can use this command:
 ```
 docker build --build-arg PHP_VERSION=8.1 --build-arg LIBRDKAFKA_VERSION=v1.8.2 --build-arg EXT_RDKAFKA_VERSION=5.0.2 --build-arg LARAVEL_VERSION=8 .
 ```
+
+You may also use the `create-image-8.sh` and `create-images.sh` scripts to generate all available images.
--- create-images-8.sh
+++ create-images-8.sh
@@ -2,11 +2,11 @@
 
 for PHP_VERSION in "8.0" "8.1"
 do
-    for LIBRDKAFKA_VERSION in "v1.8.2"
+    for LIBRDKAFKA_VERSION in "v1.6.0" "v1.6.1" "v1.6.2" "v1.7.0" "v1.8.0" "v1.8.2"
     do
-        for EXT_RDKAFKA_VERSION in "5.0.2"
+        for EXT_RDKAFKA_VERSION in "5.0.0" "5.0.1" "5.0.2"
         do
-            for LARAVEL_VERSION in "6" "7" "8"
+            for LARAVEL_VERSION in "8"
             do
                 TAG=${PHP_VERSION}-${LIBRDKAFKA_VERSION}-${EXT_RDKAFKA_VERSION}-${LARAVEL_VERSION}
 
